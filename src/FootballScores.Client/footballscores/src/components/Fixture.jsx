5fc98c4bf09e557bde3fe5410ade7dc8df680502
Hristo Dikov <dikoww7@gmail.com>
2021-06-18 22:56:45 +0300

    fixture fixed hostTeam, score, awayteam

--- src/FootballScores.Client/footballscores/src/components/Fixture.jsx
+++ src/FootballScores.Client/footballscores/src/components/Fixture.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Fixture extends Component {
   render() {
     const {
-      fixture: { gameDate, referee, venue },
+      fixture: { gameDate, referee, venue, hostTeamName, score, awayTeamName },
     } = this.props;
     return (
       <div className="game">
@@ -11,9 +11,9 @@ class Fixture extends Component {
         <div className="game-referee">{`${referee}`}</div>
         <div className="game-venue">{`${venue}`}</div>
         <div className="teams-score">
-          <div className="home-team inline-b">{this.props.hostTeamName}</div>
-          <div className="score inline-b">{this.props.score}</div>
-          <div className="away-team inline-b">{this.props.awayTeamName}</div>
+          <div className="home-team inline-b">{hostTeamName}</div>
+          <div className="score inline-b">{score}</div>
+          <div className="away-team inline-b">{awayTeamName}</div>
         </div>
       </div>
     );
